d9d676d706c189fef96d953098a5da3ae4ecbcae
Dusan Sekulic <sekula87@gmail.com>
2021-04-22 13:40:14 +0200

    secp256k1 methods tweakadd, negate, combine added (#9)
    * secp256k1 methods tweakadd, negate, combine added
    
    * tests added

--- ecdh.go
+++ ecdh.go
@@ -3,27 +3,36 @@ package secp256k1
 /*
 #include "include/secp256k1_ecdh.h"
 #cgo CFLAGS: -I${SRCDIR}/secp256k1-zkp -I${SRCDIR}/secp256k1-zkp/src
+inline secp256k1_pubkey** makePubkeyArray(int size) { return calloc(sizeof(secp256k1_pubkey*), size); }
+inline void setArrayPubkey(secp256k1_pubkey **a, secp256k1_pubkey *pubkey, int n) { a[n] = pubkey; }
+inline void freePubkeyArray(secp256k1_pubkey * *a) { free(a); }
 */
 import "C"
 
 import (
 	"errors"
+	"unsafe"
 )
 
 const (
 	// Flags for EcPubkeySerialize
-	EcCompressed = uint(C.SECP256K1_EC_COMPRESSED)
+	EcCompressed   = uint(C.SECP256K1_EC_COMPRESSED)
+	EcUncompressed = uint(C.SECP256K1_EC_UNCOMPRESSED)
 
 	// Length of elements byte representations
 	LenPrivateKey   int = 32
 	LenCompressed   int = 33
 	LenUncompressed int = 65
 
-	ErrorPrivateKeySize  string = "private key must be exactly 32 bytes"
-	ErrorEcdh            string = "unable to do ecdh"
-	ErrorPublicKeyCreate string = "unable to produce public key"
-	ErrorPublicKeySize   string = "public key must be 33 or 65 bytes"
-	ErrorPublicKeyParse  string = "unable to parse this public key"
+	ErrorPrivateKeySize     string = "private key must be exactly 32 bytes"
+	ErrorEcdh               string = "unable to do ecdh"
+	ErrorPublicKeyCreate    string = "unable to produce public key"
+	ErrorPublicKeySize      string = "public key must be 33 or 65 bytes"
+	ErrorPublicKeyParse     string = "unable to parse this public key"
+	ErrorTweakingPublicKey  string = "unable to tweak this public key"
+	ErrorTweakSize          string = "tweak must be exactly 32 bytes"
+	ErrorPublicKeyCombine   string = "unable to combine public key"
+	ErrorTweakingPrivateKey string = "unable to tweak this private key"
 )
 
 // PublicKey wraps a *secp256k1_pubkey, which contains the prefix plus
@@ -107,4 +116,82 @@ func Ecdh(ctx *Context, pubKey *PublicKey, privKey []byte) (int, []byte, error) 
 		return result, []byte{}, errors.New(ErrorEcdh)
 	}
 	return result, secret, nil
+}
+
+// Tweak a public key by adding tweak times the generator to it. The
+// return code is 0 if the tweak was out of range (chance of around 1 in
+// 2^128 for uniformly random 32-byte arrays) or if the resulting public
+// key would be invalid. The return code is 1 otherwise.
+func EcPubKeyTweakAdd(ctx *Context, pk *PublicKey, tweak []byte) (int, error) {
+	if len(tweak) != LenPrivateKey {
+		return 0, errors.New(ErrorTweakSize)
+	}
+
+	result := int(C.secp256k1_ec_pubkey_tweak_add(ctx.ctx, pk.pk, cBuf(tweak)))
+	if result != 1 {
+		return result, errors.New(ErrorTweakingPublicKey)
+	}
+	return result, nil
+}
+
+// EcPrivKeyTweakAdd modifies the provided `seckey` by adding tweak to
+// it. The return code is 0 if `tweak` was out of range (chance of
+// around 1 in 2^128 for uniformly random 32-byte arrays), or if the
+// resulting private key would be invalid (only when the tweak is the
+// complement of the private key). The return code is 1 otherwise.
+func EcPrivKeyTweakAdd(ctx *Context, seckey []byte, tweak []byte) (int, error) {
+	if len(tweak) != LenPrivateKey {
+		return 0, errors.New(ErrorTweakSize)
+	}
+	if len(seckey) != LenPrivateKey {
+		return 0, errors.New(ErrorPrivateKeySize)
+	}
+
+	result := int(C.secp256k1_ec_privkey_tweak_add(ctx.ctx, (*C.uchar)(unsafe.Pointer(&seckey[0])), cBuf(tweak[:])))
+	if result != 1 {
+		return result, errors.New(ErrorTweakingPrivateKey)
+	}
+	return result, nil
+}
+
+// EcPubKeyNegate will negate a public key object in place. The return code
+// is always 1.
+func EcPubKeyNegate(ctx *Context, pubkey *PublicKey) (int, error) {
+	result := int(C.secp256k1_ec_pubkey_negate(ctx.ctx, pubkey.pk))
+	return result, nil
+}
+
+// EcPrivKeyNegate will negate a public key in place. The return code is
+// 1 if the operation was successful, or 0 if the length was invalid.
+func EcPrivKeyNegate(ctx *Context, seckey []byte) (int, error) {
+	if len(seckey) != LenPrivateKey {
+		return 0, errors.New(ErrorPrivateKeySize)
+	}
+
+	result := int(C.secp256k1_ec_privkey_negate(ctx.ctx, (*C.uchar)(unsafe.Pointer(&seckey[0]))))
+	return result, nil
+}
+
+// EcPubKeyCombine will compute sum of all the provided public keys,
+// returning a new point. The error code is 1 if the sum is valid, 0
+// otherwise. There must be at least one public key.
+func EcPubKeyCombine(ctx *Context, vPk []*PublicKey) (int, *PublicKey, error) {
+	l := len(vPk)
+	if l < 1 {
+		return 0, nil, errors.New("must provide at least one public key")
+	}
+
+	array := C.makePubkeyArray(C.int(l))
+	for i := 0; i < l; i++ {
+		C.setArrayPubkey(array, vPk[i].pk, C.int(i))
+	}
+
+	defer C.freePubkeyArray(array)
+
+	pkOut := newPublicKey()
+	result := int(C.secp256k1_ec_pubkey_combine(ctx.ctx, pkOut.pk, array, C.size_t(l)))
+	if result != 1 {
+		return result, nil, errors.New(ErrorPublicKeyCombine)
+	}
+	return result, pkOut, nil
 }
--- ecdh_test.go
+++ ecdh_test.go
@@ -55,3 +55,120 @@ func TestEcdh(t *testing.T) {
 	}
 
 }
+
+func TestPubKeyTweakAddFixtures(t *testing.T) {
+	ctx, err := secp256k1.ContextCreate(secp256k1.ContextSign | secp256k1.ContextVerify)
+	if err != nil {
+		panic(err)
+	}
+
+	file, err := ioutil.ReadFile("testdata/pubkey_tweak_add_vectors.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var tests map[string]interface{}
+	err = json.Unmarshal(file, &tests)
+	if err != nil {
+		t.Error(err)
+	}
+
+	vectors := tests["tweak_add"].([]interface{})
+
+	for _, testVector := range vectors {
+		v := testVector.(map[string]interface{})
+		pubKeyBytes, err := hex.DecodeString(v["publicKey"].(string))
+		if err != nil {
+			t.Error(err)
+		}
+		_, pubKey, err := secp256k1.EcPubkeyParse(ctx, pubKeyBytes)
+		if err != nil {
+			t.Error(err)
+		}
+
+		tweakBytes, err := hex.DecodeString(v["tweak"].(string))
+		if err != nil {
+			t.Error(err)
+		}
+
+		r, err := secp256k1.EcPubKeyTweakAdd(ctx, pubKey, tweakBytes)
+		spOK(t, r, err)
+
+		r, serialized, err := secp256k1.EcPubkeySerialize(ctx, pubKey, secp256k1.EcUncompressed)
+		spOK(t, r, err)
+
+		tweakedBytes, err := hex.DecodeString(v["tweaked"].(string))
+		if err != nil {
+			panic(err)
+		}
+
+		assert.Equal(t, tweakedBytes, serialized)
+	}
+}
+
+func TestPubKeyCombine(t *testing.T) {
+	ctx, err := secp256k1.ContextCreate(secp256k1.ContextSign | secp256k1.ContextVerify)
+	if err != nil {
+		panic(err)
+	}
+
+	privkey := []byte(`abcd1234abcd1234abcd1234abcd1234`)
+
+	// privkey * G
+	r, pubkey, err := secp256k1.EcPubkeyCreate(ctx, privkey)
+	spOK(t, r, err)
+
+	// tweak * G
+	tweak := []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1}
+	r, tweakPub, err := secp256k1.EcPubkeyCreate(ctx, tweak)
+	spOK(t, r, err)
+
+	// tweakedPriv: privkey + tweak
+	// tweakedToPoint: (privkey+tweak) * G
+	tweakedPriv := privkey
+	r, err = secp256k1.EcPrivKeyTweakAdd(ctx, tweakedPriv, tweak)
+	spOK(t, r, err)
+	r, tweakedToPoint, err := secp256k1.EcPubkeyCreate(ctx, tweakedPriv)
+	spOK(t, r, err)
+
+	vPoint := []*secp256k1.PublicKey{pubkey, tweakPub}
+	r, combinedPoint, err := secp256k1.EcPubKeyCombine(ctx, vPoint)
+	spOK(t, r, err)
+
+	assert.Equal(t, tweakedToPoint, combinedPoint)
+}
+
+func TestPubKeyNegate(t *testing.T) {
+	ctx, err := secp256k1.ContextCreate(secp256k1.ContextSign | secp256k1.ContextVerify)
+	if err != nil {
+		panic(err)
+	}
+
+	privkey := []byte(`abcd1234abcd1234abcd1234abcd1234`)
+
+	// (-1*k)*G == -1*(k*G)
+	privkeyCopy := []byte(`abcd1234abcd1234abcd1234abcd1234`)
+	r, err := secp256k1.EcPrivKeyNegate(ctx, privkeyCopy)
+	spOK(t, r, err)
+	r, LHS, err := secp256k1.EcPubkeyCreate(ctx, privkeyCopy)
+	spOK(t, r, err)
+
+	r, rhs, err := secp256k1.EcPubkeyCreate(ctx, privkey)
+	spOK(t, r, err)
+	r, err = secp256k1.EcPubKeyNegate(ctx, rhs)
+	assert.Equal(t, 1, r)
+	assert.NoError(t, err)
+
+	assert.Equal(t, LHS, rhs)
+
+}
+
+func spOK(t *testing.T, result interface{}, err error) {
+	assert.NoError(t, err)
+	switch result := result.(type) {
+	case int:
+		assert.Equal(t, 1, result)
+	case bool:
+		assert.True(t, result)
+	}
+}
